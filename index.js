73ba3e24b6392248d7140c027eb7b1f0292dd425
Tauhid <tauhid.swe@gmail.com>
2023-03-10 15:02:11 +0600

    made an api for update an specific order

--- index.js
+++ index.js
@@ -20,17 +20,17 @@ async function run(){
 
         app.get('/services', async(req, res)=>{
             const query = {};
-            const cursor = servicesCollection.find(query)
+            const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
         app.get('/services/:id', async(req, res)=>{
             const id = req.params.id;
             const query = { _id: new ObjectId(id)};
             const service = await servicesCollection.findOne(query);
             res.send(service);
-        })
+        });
         
         //order api
         app.get('/orders', async(req, res)=>{
@@ -43,11 +43,24 @@ async function run(){
             const cursor = orderCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
-        })
+        });
 
         app.post('/orders', async(req, res)=>{
             const order = req.body;
             const result = await orderCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.patch('/orders/:id', async(req, res)=>{
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = {_id: new ObjectId(id)}
+            const updatedDoc = {
+                $set: {
+                    status :  status
+                }
+            }
+            const result = await orderCollection.updateOne(query,updatedDoc);
             res.send(result);
         })
 
@@ -56,7 +69,7 @@ async function run(){
             const query = {_id: new ObjectId(id)};
             const result = await orderCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
     }
 
