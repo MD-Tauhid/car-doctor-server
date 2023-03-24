152b79d6bea40159849465be13d0c35736c8c5a5
Tauhid <tauhid.swe@gmail.com>
2023-03-24 20:13:12 +0600

    Modify the api which provide authentication bearer token into the header

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,29 +14,56 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ybh5qdc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+async function run() {
+    try {
         const servicesCollection = client.db('carDoctor').collection('services');
         const orderCollection = client.db('carDoctor').collection('orders');
 
-        app.get('/services', async(req, res)=>{
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token });
+        });
+
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
-        app.get('/services/:id', async(req, res)=>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.send(service);
         });
-        
+
         //order api
-        app.get('/orders', async(req, res)=>{
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
+
             let query = {};
-            if(req.query.email){
+            if (req.query.email) {
                 query = {
                     email: req.query.email
                 }
@@ -45,45 +73,45 @@ async function run(){
             res.send(result);
         });
 
-        app.post('/orders', async(req, res)=>{
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         });
 
-        app.patch('/orders/:id', async(req, res)=>{
+        app.patch('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const status = req.body.status;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const updatedDoc = {
                 $set: {
-                    status :  status
+                    status: status
                 }
             }
-            const result = await orderCollection.updateOne(query,updatedDoc);
+            const result = await orderCollection.updateOne(query, updatedDoc);
             res.send(result);
         })
 
-        app.delete('/orders/:id', async(req, res)=>{
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         });
 
     }
 
-    finally{
+    finally {
 
     }
 }
 run().catch(err => console.error(err));
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Car doctor server is running');
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Car Doctor server is running on port: ${port}`);
 })
